96ab21fe43286d96c5bb6881cdd62ddd46534063
robin <robinhartmann@live.com>
2024-09-23 19:23:32 +0200

    implement sizeof.c excercise program

---
+++ .ccls-cache/@home@robin@syspr@fhnw-syspr-work-00-RobinErHa/sizeof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("char size: %ld\n", sizeof(char));
+	printf("int size: %ld\n", sizeof(int));
+	printf("long size: %ld\n", sizeof(long));
+	printf("float size: %ld\n", sizeof(float));
+	printf("double size: %ld\n", sizeof(double));
+	return 0;
+}
