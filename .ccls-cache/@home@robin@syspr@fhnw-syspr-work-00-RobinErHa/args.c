5203d2ce8aac68c01f90d5e06291a08166314325
robin <robinhartmann@live.com>
2024-09-24 17:01:32 +0200

    add args.c excercise completed

---
+++ .ccls-cache/@home@robin@syspr@fhnw-syspr-work-00-RobinErHa/args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+
+	for (int i = 1; i < argc ; i++) {
+		for (char *p = argv[i]; *p != '\0'; p++) {
+			if (*p < 'a' || *p > 'z') { 
+				printf("Invalid input: %c", *p);
+				return 1;
+			}
+		}
+		printf("%s \t", argv[i]);
+	}
+
+}
